07901e5d8937c997ee8b95c4e929e85642cc394f
saurabhasnare <66157342+saurabhasnare@users.noreply.github.com>
2021-12-18 00:10:37 +0530

    Remote sql connected

--- connection/connect.php
+++ connection/connect.php
@@ -1,10 +1,16 @@
 <?php
+//remote connection 
+$servername = " sql6.freemysqlhosting.net"; //server
+$username = "sql6459698"; //username
+$password = "gXHpJLqVZp"; //password
+$dbname = "sql6459698";  //database
 
+//devlopment connection
 //main connection file for both admin & front end
-$servername = "localhost"; //server
-$username = "root"; //username
-$password = ""; //password
-$dbname = "online_rest";  //database
+// $servername = "localhost"; //server
+// $username = "root"; //username
+// $password = ""; //password
+// $dbname = "online_rest";  //database
 
 // Create connection
 $db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
